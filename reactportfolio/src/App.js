eaec1e3a6f17af8b9b046efe8d419da8bcdd2eb5
Sterling Etienne <sterlingetienne@Sterlings-MacBook-Air.local>
2020-05-12 00:16:13 -0400

    landing page

--- reactportfolio/src/App.js
+++ reactportfolio/src/App.js
@@ -1,41 +1,38 @@
 import React, { Component } from 'react';
 import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
 import Main from "../src/components/main";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import './App.css';
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
-      <div>
-        <div style={{ height: '300px', position: 'relative' }}>
-          <Layout style={{ background: 'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover' }}>
-            <Header transparent title="Title" style={{ color: 'white' }}>
-              <Navigation>
-                <Link to="/resume">Resume</Link>
-                <Link to="/aboutMe">About Me</Link>
-                <Link to="/project">Projects</Link>
-                <Link to="/contact">Contact</Link>
-              </Navigation>
-            </Header>
-            <Drawer title="Title">
-              <Navigation>
-                <a to="#">Link</a>
-                <a to="#">Link</a>
-                <a to="#">Link</a>
-                <a to="#">Link</a>
-              </Navigation>
-            </Drawer>
-            <Content />
-          </Layout>
-        </div>
-        <Content>
-          <div className="page-content" />
-          <Main />
-        </Content>
-
+      <div className="layout">
+        <Layout>
+          <Header title="Portfolio" scroll>
+            <Navigation>
+              <Link to="/resume">Resume</Link>
+              <Link to="/aboutMe">About Me</Link>
+              <Link to="/project">Projects</Link>
+              <Link to="/contact">Contact</Link>
+            </Navigation>
+          </Header>
+          <Drawer title="Portfolio">
+            <Navigation>
+              <a href="#">Link</a>
+              <a href="#">Link</a>
+              <a href="#">Link</a>
+              <a href="#">Link</a>
+            </Navigation>
+          </Drawer>
+          <Content>
+            <div className="page-content" />
+            <Main />
+          </Content>
+        </Layout>
       </div>
+
     )
   };
 }
--- reactportfolio/src/components/landingPage.js
+++ reactportfolio/src/components/landingPage.js
@@ -1,10 +1,41 @@
 import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
 
 class Landing extends Component {
     render() {
         return (
-            <div>
-                <h1> Landing Page</h1>
+            <div style={{ width: "100%", margin: "auto", position: 'relative'}} >
+                <Grid className="land-grid">
+                    <Cell col="12">
+                        <div className="pic">
+                        <img 
+                        src="./pic/sterling.jpg"
+                        alt="sterling"
+                        className="sterling-pic" 
+                        />
+                        </div>
+                        <div className="pic-text">
+                            <h1>  Full Stack Web Developer</h1>
+                            <hr/>
+                            <p> HTML/CSS | Javascript | React | NodeJs | MongoDB | MySQL </p>
+
+                            <div className="social-media">
+                            {/* linkedIn */}
+                            <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                            <i className="fa fa-linkedin-square" aria-hidden="true" />
+                            </a>
+                              {/* GithUb */}
+                              <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+                            <i className="fa fa-github-square" aria-hidden="true" />
+                            </a>
+                             
+                        </div>
+                        </div>
+
+                        
+
+                    </Cell>
+                </Grid>
             </div>
         )
     }
